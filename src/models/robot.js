f64ab98bdba8755644df071fbfb6cc53e3ddf2a3
Anonymous <yuchao.sydneyuni@gmail.com>
2018-09-07 11:28:39 +1000

    Add Robot class with test and add coverage to repo

---
+++ src/models/robot.js
@@ -0,0 +1,47 @@
+class Robot {
+  
+  constructor(x, y, f) {
+    this.x = x;
+    this.y = y;
+    this.f = f;
+  }
+
+  get x() {
+    return this._x;
+  }
+
+  get y() {
+    return this._y;
+  }
+
+  get f() {
+    return this._f;
+  }
+
+  set x(value) {
+    if (typeof value !== 'number') {
+      throw new Error('"x" must be a number.');
+    }
+
+    this._x = value;
+  }
+
+  set y(value) {
+    if (typeof value !== 'number') {
+      throw new Error('"y" must be a number.');
+    }
+
+    this._y = value;
+  }
+
+  set f(value) {
+    if (typeof value !== 'string') {
+      throw new Error('"f" must be a string.');
+    }
+
+    this._f = value;
+  }
+
+}
+
+module.exports = Robot;
---
+++ src/models/robot.test.js
@@ -0,0 +1,89 @@
+'use strict'
+
+const chai = require('chai');
+const path = require('path');
+const expect = chai.expect;
+
+chai.should();
+
+const Robot = require(path.join(__dirname, '.', 'robot'));
+
+describe('Class: Robot', () => {
+  describe('#x', () => {
+    let robot;
+
+    beforeEach(() => {
+      // create a robot
+      robot = new Robot(0,0,'NORTH');
+    });
+
+    it('returns location x', () => {
+      robot.x.should.equal(0);
+    });
+
+    it('can be changed', () => {
+      // Assert that x can be changed
+      robot.x = 1;
+      robot.x.should.equal(1);
+    });
+
+    it('only accepts numerical values', () => {
+      // Assert that an error will be thrown if
+      // the x it set to a non-numerical value.
+      expect(() => robot.x = 'random').to.throw('"x" must be a number.');
+    });
+
+  });
+
+  describe('#y', () => {
+    let robot;
+
+    beforeEach(() => {
+      // create a robot
+      robot = new Robot(0,0,'NORTH');
+    });
+
+    it('returns location y', () => {
+      robot.y.should.equal(0);
+    });
+
+    it('can be changed', () => {
+      // Assert that y can be changed
+      robot.y = 1;
+      robot.y.should.equal(1);
+    });
+
+    it('only accepts numerical values', () => {
+      // Assert that an error will be thrown if
+      // the y it set to a non-numerical value.
+      expect(() => robot.y = 'random').to.throw('"y" must be a number.');
+    });
+
+  });
+
+  describe('#f', () => {
+    let robot;
+
+    beforeEach(() => {
+      // create a robot
+      robot = new Robot(0,0,'NORTH');
+    });
+
+    it('returns location f', () => {
+      robot.f.should.equal('NORTH');
+    });
+
+    it('can be changed', () => {
+      // Assert that f can be changed
+      robot.f = 'SOUTH';
+      robot.f.should.equal('SOUTH');
+    });
+
+    it('onlf accepts string values', () => {
+      // Assert that an error will be thrown if
+      // the f it set to a non-string value.
+      expect(() => robot.f = 1).to.throw('"f" must be a string.');
+    });
+
+  });
+});
