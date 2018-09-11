885932a6680c9f35aa35f2a312e39c38e8cc37e7
Anonymous <yuchao.sydneyuni@gmail.com>
2018-09-11 21:52:32 +1000

    Make production release publish the app as a gloable cli command

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/app.js',
@@ -7,5 +8,8 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new webpack.BannerPlugin({ banner: "#!/usr/bin/env node", raw: true })
+  ]
 };
